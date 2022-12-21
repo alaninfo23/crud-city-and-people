3cff7d3e1335865fa4bdedb5dbb52cc129b926b0
Andre Lucas <andrelucca99@gmail.com>
2022-12-21 14:02:44 -0300

    feat: add barra de busca

--- src/page/dashboard/DashBord.tsx
+++ src/page/dashboard/DashBord.tsx
@@ -1,8 +1,17 @@
+import { BarraDeFerramentas } from '../../shared/components';
 import { LayoutBaseDePagina } from '../../shared/layouts';
 
 export const Dashboard = () => {
   return (
-    <LayoutBaseDePagina titulo="Página inicial" barraDeFerramentas={<>Barra de ferramentas</>}>
+    <LayoutBaseDePagina
+      titulo="Página inicial"
+      barraDeFerramentas={(
+        <BarraDeFerramentas
+          mostrarInputBusca
+          textoBotaoNovo='Novo'
+        />
+      )}
+    >
         Testando
     </LayoutBaseDePagina>
   );
