faa50ee1eabee82bb69c203c2cf8b207efda4eb1
Andre Lucas <andrelucca99@gmail.com>
2022-12-22 13:19:02 -0300

    feat: add variáveis de ambiente

--- src/shared/environment/index.ts
+++ src/shared/environment/index.ts
@@ -1,1 +1,18 @@
-export { };
+export const Environment = {
+  /**
+   * Define a quantidade de linhas a ser carregada pro padrão nas listagem
+   */
+  LIMITE_DE_LINHAS: 10,
+  /**
+   * Placeholder exibido nas inputs
+   */
+  INPUT_DE_BUSCA: 'Perquisar...',
+  /**
+   * Texto exibido quando nenhum registro é encontrado em uma listagem
+   */
+  LISTAGEM_VAZIA: 'Nenhum registro encontrado.',
+  /**
+   * Url base de consulta dos dados dessa aplicação 
+   */
+  URL_BASE: 'http://localhost:3333',
+};
